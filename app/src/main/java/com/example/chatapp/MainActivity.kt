d18fb3f111d4d1867e68ad0ce075a4cb86397629
Trần Nhật Linh <trannhatlinh120801@gmail.com>
2022-08-30 16:53:20 +0700

    fix bug

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.example.chatapp
 
-import android.graphics.Color
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
-import android.view.View
-import android.view.WindowManager
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -20,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     private var mAuth: FirebaseAuth? = null
-    private var countryCode: String = "+"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,23 +29,4 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun sendVerificationCode(phone: String) {
-        val options = PhoneAuthOptions.newBuilder(mAuth!!)
-            .setPhoneNumber(phone)
-            .setTimeout(60L, TimeUnit.SECONDS)
-            .setActivity(this)
-            .setCallbacks(object :
-                PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
-
-                }
-
-                override fun onVerificationFailed(p0: FirebaseException) {
-
-                }
-
-            })
-            .build()
-        PhoneAuthProvider.verifyPhoneNumber(options)
-    }
 }
