b35d8cf7dc3fb3593737d8d44559e2ee13584a21
Trần Nhật Linh <trannhatlinh120801@gmail.com>
2022-09-15 10:59:18 +0700

    add bottom nav and code chat activity

--- app/src/main/java/com/example/chatapp/ui/act_chat/activity/ChatActivity.kt
+++ app/src/main/java/com/example/chatapp/ui/act_chat/activity/ChatActivity.kt
@@ -2,11 +2,27 @@ package com.example.chatapp.ui.act_chat.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.chatapp.R
+import com.example.chatapp.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        supportActionBar?.hide()
+
+        val binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.chat_activity_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
